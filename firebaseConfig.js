dd25a6e01b9cbdffe52af47c82bf4fc4ec6c8e18
Tabasa-J <johnalberttabasa10@gmail.com>
2025-08-28 11:43:13 +0800

    Firebase Configiration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfSTHjmK20HZXaT4mp4GxF7IgBoo7xiM8",
+  authDomain: "jat-goal-tracker.firebaseapp.com",
+  projectId: "jat-goal-tracker",
+  storageBucket: "jat-goal-tracker.firebasestorage.app",
+  messagingSenderId: "390842785545",
+  appId: "1:390842785545:web:9f982f5ad0009c2dd7fdd8"
+};
+
+const app = initializeApp(firebaseConfig);
